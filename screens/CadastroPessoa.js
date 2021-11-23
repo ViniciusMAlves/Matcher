fd00238aa3eaa5bfa631be7ae05263e514f18938
ViniciusMAlves <37082519+ViniciusMAlves@users.noreply.github.com>
2021-11-22 22:31:07 -0300

    comandos SQL
    começando a colocar comandos SQL

--- screens/CadastroPessoa.js
+++ screens/CadastroPessoa.js
@@ -2,7 +2,39 @@ import * as React from 'react';
 import { View, StyleSheet, Text } from 'react-native';
 import { TextInput, Button } from 'react-native-paper';
 
+import openDB from "../db";
+const db = openDB();
+
 export default function CadastroPessoa({ navigation }) {
+    const [pessoa, setPESSOAS] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    function saveUsuario(pessoa) {
+        db.transaction(tx => {
+        tx.executeSql("INSERT INTO PESSOAS (USER, EMAIL, PASSWORD, IMG_PESSOA) VALUES(?, ?)", [pessoa.user, pessoa.email, pessoa.password, pessoa.img_pessoa], (_, rs) => {
+            console.log(`Novo usuario salvo: ${rs.insertId}`);
+            recuperaUsuarios();
+        });
+        });
+    }
+
+    function removeUsuario(id) {
+        db.transaction(tx => {
+        tx.executeSql("DELETE FROM PESSOAS WHERE ID_PESSOA = ?", [id], (_, rs) => {
+            recuperaUsuarios();
+        });
+        });
+    }
+
+    function recuperaUsuarios(id) {
+        db.transaction(tx => {
+        tx.executeSql("SELECT * FROM PESSOAS WHERE ID_PESSOA = ?", [id], (_, rs) => {
+            setPESSOAS(rs.rows._array);
+            setLoading(false);
+        });
+        });
+    }
+
     return(
         <View style={styles.container}>
             <TextInput
--- screens/CadastroProd.js
+++ screens/CadastroProd.js
@@ -2,7 +2,40 @@ import * as React from 'react';
 import { View, StyleSheet, Text } from 'react-native';
 import { TextInput, Button } from 'react-native-paper';
 
+import openDB from "../db";
+const db = openDB();
+
 export default function CadastroProd({ navigation }) {
+    const [produtos, setPRODUTOS] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    function saveProduto(produtos) {
+        db.transaction(tx => {
+        tx.executeSql("INSERT INTO PRODUTOS (ID_PESSOA, NOME, QUANT, PRECO_ANT, PRECO_ATU, OBS, IMG_PROD) VALUES(?, ?)", [produtos.id_pessoa, produtos.nome, produtos.quant, produtos.preco_ant, produtos.preco_atu, produtos.obs, produtos.img_prod], (_, rs) => {
+            console.log(`Novo usuario salvo: ${rs.insertId}`);
+            recuperaProduto();
+        });
+        });
+    }
+
+    function removeProduto(id) {
+        db.transaction(tx => {
+        tx.executeSql("DELETE FROM PRODUTOS WHERE ID_PRODUT = ?", [id], (_, rs) => {
+            recuperaProduto();
+        });
+        });
+    }
+
+    function recuperaProduto(id) {
+        db.transaction(tx => {
+        tx.executeSql("SELECT * FROM PRODUTOS WHERE ID_PRODUT = ?", [id], (_, rs) => {
+            setProduto(rs.rows._array);
+            setLoading(false);
+        });
+        });
+    }
+
+
     return(
         <View style={styles.container}>
             <TextInput
