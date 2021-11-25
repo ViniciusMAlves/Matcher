ad12d69c3486bfbb21dd0123778388b21deb2383
ViniciusMAlves <37082519+ViniciusMAlves@users.noreply.github.com>
2021-11-25 07:32:27 -0300

    modificado o app.js
    modificado o layout

--- App.js
+++ App.js
@@ -2,8 +2,6 @@ import React from "react";
 import { Provider as PaperProvider } from "react-native-paper";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-
 
 import LoginScreen from "./screens/Login";
 import CadastroPessoaScreen from "./screens/CadastroPessoa";
@@ -14,22 +12,28 @@ import ListaProdScreen from "./screens/ListaProd";
 import ProdutoScreen from "./screens/Produto";
 import UsuarioScreen from "./screens/Usuario";
 
-
-const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
 
 function App() {
   return (
-    <NavigationContainer>
-      <Tab.Navigator>
-      <Tab.Screen name="Login" component={LoginScreen} options={{  headerShown: false, title: "Login" }} />
-      <Tab.Screen name="CadastroPessoa" component={CadastroPessoaScreen} options={{ title: "Sing in" }} />
-      <Tab.Screen name="CadastroProd" component={CadastroProdScreen} options={{ title: "Cadastro Prod." }} />
-      </Tab.Navigator>
+    <NavigationContainer styles={{display: "none"}}>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false, title: "Login" }} />
+        <Stack.Screen name="CadastroPessoa" component={CadastroPessoaScreen} options={{title: "Sing in" }} />
+        <Stack.Screen name="CadastroProd" component={CadastroProdScreen} options={{ title: "Cadastro Prod." }} />
+        <Stack.Screen name="Usuario" component={UsuarioScreen} options={{ title: "Usuário" }} />
+        <Stack.Screen name="Produto" component={ProdutoScreen} options={{ title: "Produto" }} />
+        <Stack.Screen name="ListaProduto" component={ListaProdScreen} options={{ title: "Lista de Produtos" }} />
+        <Stack.Screen name="CriarCatalogo" component={CriaCatalogoScreen} options={{ title: "Criar Catálogo" }} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
+  
 }
 
+
 export default function ThemedApp() {
+
   return (
     <PaperProvider>
       <App />
